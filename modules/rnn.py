fix RNN gradients

modules/rnn.py
@@ -41,7 +41,7 @@ class PyRNN(nn.Module):
                     x_layer @ self.WI[layer].T + self.BI[layer] +
                     h_t_minus_1[layer] @ self.WH[layer].T + self.BH[layer]
                 ) # don't know why but pytorch's implementation has two learnable biases... would assume that they could be combined into one
-            output.append(h_t[-1].clone())
+            output.append(h_t[-1].clone().detach())
 
         output = torch.stack(output, dim=1)
         return output, h_t