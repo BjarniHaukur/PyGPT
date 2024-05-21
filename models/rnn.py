e16a4f97429328011e6ee1e20326a2c3e52d86ee
Jonas Rosengren <rosengrenjonas98@gmail.com>
2024-05-21 14:10:34 +0200

    nucleus with temp

--- models/rnn.py
+++ models/rnn.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from utils.sample import nucleus_sample
+from utils.sample import nucleus_sample_with_temp
 from utils.tokenizer import BOS_ID, EOS_ID
 
 class PyRNN(nn.Module):
@@ -21,20 +21,25 @@ class PyRNN(nn.Module):
     
 
     @torch.no_grad()
-    def generate(self, max_len=1000, starting_tokens:list[int]=None)->str:
+    def generate(self, max_len=1000, starting_tokens:list[int]=None,  nucleus_threshold:float=None, temperature:float=None)->str:
         """ Generates a sequence of tokens """
         self.eval()
         device = next(self.parameters()).device
         xt = torch.tensor([[BOS_ID] + (starting_tokens or [])], device=device)
         ht = torch.randn(1, 1, self.hidden_size, device=device)
+
+        sample_kwargs = {}
+        if nucleus_threshold is not None:
+            sample_kwargs['nucleus_threshold'] = nucleus_threshold
+        if temperature is not None:
+            sample_kwargs['temperature'] = temperature
         
         tokens = starting_tokens or []
         for _ in range(max_len):
             xt = self.embed(xt)
             xt, ht = self.rnn(xt, ht)
             xt = self.linear(xt)
-            xt = nucleus_sample(xt[:,-1,:], nucleus_threshold=0.9)
-            # xt = sample_with_temp(xt[:,-1,:], temperature=1.0)
+            xt = nucleus_sample_with_temp(xt[:,-1,:], **sample_kwargs)
             token = xt.item()
             if token == EOS_ID:
                 break
