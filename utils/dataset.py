e54d971fb4e642fdd650bafc1ed2fa6fb4e56311
Fabio Sabbion <fabiosabbion.apps@gmail.com>
2024-05-21 17:03:02 +0200

    added encoding utf-8 to the file open function

--- utils/dataset.py
+++ utils/dataset.py
@@ -12,7 +12,7 @@ from torch.utils.data import Dataset
 class Py150kDataset(Dataset):
     """ Reads and tokenizes each file in the PY150k dataset. """
     def __init__(self, split:Literal["train","eval"], tokenizer_name:str):
-        self.files = open("data/PY150k/python" + ("100k_train.txt" if split=="train" else "50k_eval.txt"), "r").read().split("\n")[:-1] # last is empty line
+        self.files = open("data/PY150k/python" + ("100k_train.txt" if split=="train" else "50k_eval.txt"), "r", encoding='utf-8').read().split("\n")[:-1] # last is empty line
         self.tokenizer = BPETokenizer.load(tokenizer_name)
 
     @lru_cache() # creates a dictionary behind the scenes which maps idx to the data, i.e. only tokenize once
